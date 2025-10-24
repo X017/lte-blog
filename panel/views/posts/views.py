9b9661b0345d6a2b423e49427c07d615e78b7c76
Parsa <parsam280xyz@gmail.com>
2025-10-25 00:47:59 +0330

    fix:added permissions for each view

--- panel/views/posts/views.py
+++ panel/views/posts/views.py
@@ -1,59 +1,61 @@
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, DetailView, ListView, UpdateView , View
+from django.views.generic import CreateView, DetailView, ListView, UpdateView, View
 from django.shortcuts import get_object_or_404, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from post.models import Comment, Post
 from panel.views.posts.forms import BlogForm
 
-class PostListView(LoginRequiredMixin,ListView):
+class PostListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Post
     template_name = 'panel/posts/list.html'
     context_object_name = 'posts'
+    permission_required = ['post.view_post']
 
-
-class PostCreateView(LoginRequiredMixin,CreateView):
+class PostCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
     model = Post
     template_name = 'panel/posts/create.html'
     form_class = BlogForm
     success_url = reverse_lazy('post:list')
-
+    permission_required = ['post.add_post']
+    
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UpdateView):
+class PostUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
     model = Post
     template_name = 'panel/posts/create.html'
     form_class = BlogForm
-
     success_url = reverse_lazy('post:list')
+    permission_required = ['post.change_post']
 
-class PostDetailView(LoginRequiredMixin,DetailView):
+class PostDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Post 
     template_name = 'panel/posts/detail.html'
     form_class = BlogForm
     context_object_name = 'post'
-
+    permission_required = ['post.view_post']
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['comments'] = Comment.objects.filter(post=self.object)
         return context
 
-class PostDeleteView(LoginRequiredMixin,View):
-    def get(self,request,pk,**kwargs):
-        obj = get_object_or_404(Post,pk=pk)
+class PostDeleteView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    permission_required = ['post.delete_post']
+    
+    def get(self, request, pk, **kwargs):
+        obj = get_object_or_404(Post, pk=pk)
         obj.delete()
         return redirect(reverse_lazy('post:list'))
 
-
-
-
-class PostDashboardListView(LoginRequiredMixin,ListView):
+class PostDashboardListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     model = Post
     template_name = 'panel/posts/dashboard.html'      
     context_object_name = 'posts'
     ordering = ['-created_at']
-
+    permission_required = ['post.view_post']
+    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Annotate each post with its comments (like your DetailView)
