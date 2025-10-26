c9a8ac75fc4f8f30cd888844639fb15a4744d482
Parsa <parsam280xyz@gmail.com>
2025-10-26 09:34:22 +0330

    fix:added permission mixin for user views and comment views

--- panel/views/comments/views.py
+++ panel/views/comments/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from panel.views.comments.forms import CommentsForm
 from post.models import Comment 
@@ -6,32 +6,41 @@ from django.views.generic import ListView, CreateView,UpdateView,View,DetailView
 from django.shortcuts import get_object_or_404, redirect
 
 
-class CommentListView(LoginRequiredMixin,ListView):
+class CommentListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
     model = Comment 
     template_name = 'panel/comments/list.html'
     context_object_name = 'comments'
+    permission_required = ['perms.post.view_comment']
 
-class CommentCreateView(LoginRequiredMixin,CreateView):
+class CommentCreateView(LoginRequiredMixin,PermissionRequiredMixin,CreateView):
     model = Comment
     template_name = 'panel/comments/create.html'
     form_class = CommentsForm
+    permission_required = ['perms.post.add_comment']
     success_url = reverse_lazy('comments:list')
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-class CommentUpdateView(LoginRequiredMixin,UpdateView):
+
+class CommentUpdateView(LoginRequiredMixin,PermissionRequiredMixin,UpdateView):
     model = Comment
     template_name = 'panel/comments/update.html'
     form_class = CommentsForm
+    permission_required = ['perms.post.change_comment']
     success_url = reverse_lazy('comments:list')
 
-class CommentDetailView(LoginRequiredMixin,DetailView):
+class CommentDetailView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     model = Comment
     template_name = 'panel/comments/detail.html'
     form_class = CommentsForm 
 
+    permission_required = ['perms.post.view_comment']
 
-class CommentDeleteView(LoginRequiredMixin,View):
+
+class CommentDeleteView(LoginRequiredMixin,PermissionRequiredMixin,View):
+
+    permission_required = ['perms.post.delete_comment']
     def get(self,request,pk,**kwargs):
         obj = get_object_or_404(Comment,pk=pk)
         obj.delete()
--- panel/views/users/views.py
+++ panel/views/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, ListView, UpdateView, View 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -13,10 +13,11 @@ from post.models import Comment, Post
 
 User = get_user_model()
 
-class UserProfileView(LoginRequiredMixin,DetailView):
+class UserProfileView(LoginRequiredMixin,PermissionRequiredMixin,DetailView):
     model = User 
     template_name = 'panel/users/profile.html'
     context_object_name = 'user'
+    permission_required = ['perms.auth.view_user']
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -25,25 +26,29 @@ class UserProfileView(LoginRequiredMixin,DetailView):
         return context
 
 
-class UserListView(LoginRequiredMixin,ListView):
+class UserListView(LoginRequiredMixin,PermissionRequiredMixin,ListView):
     model = User 
     template_name = 'panel/users/list.html'
     context_object_name = 'users'
+    permission_required = ['perms.auth.view_user']
 
-class UserCreateView(CreateView):
+class UserCreateView(LoginRequiredMixin,PermissionRequiredMixin,CreateView):
     model = User
     form_class = UserModelForm
     template_name = 'panel/users/create.html'
     success_url = reverse_lazy('users:list')
+    permission_required = ['perms.auth.add_user']
 
-class UserDeleteView(LoginRequiredMixin,View):
-    model = User 
+class UserDeleteView(LoginRequiredMixin,PermissionRequiredMixin,View):
+    permission_required = ['perms.auth.delete_user']
     def get(self,request,pk):
         obj = get_object_or_404(User,pk=pk)
         obj.delete()
 
-class UserUpdateView(LoginRequiredMixin,UpdateView):
+class UserUpdateView(LoginRequiredMixin,PermissionRequiredMixin,UpdateView):
     model = User 
     template_name = 'panel/users/create.html'
     form_class = UserModelForm
+    permission_required = ['perms.auth.change_user']
     success_url = reverse_lazy('users:list')
+
